57a1007b8321b1ea3906c38c74bc7ad41e17fb91
Abdul Fazil <abdulfazil109@gmail.com>
2025-07-19 01:34:34 +0530

    Initial commit with .gitignore

---
+++ Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseTrackerAPI.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Titile { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
